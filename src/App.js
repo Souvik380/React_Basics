5007b92f0a4fe145d310c95ad54058a613e43f04
Souvik Mukherjee <craigdaniel144@gmail.com>
2022-11-06 00:40:37 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import {useState} from "react"
 import './App.css';
 
 function App() {
+
+  const [students,setStudents]=useState([
+    {name:"Souvik",score:24},
+    {name:"Sampad",score:45},
+    {name:"Rajat",score:40},
+    {name:"Sumeet",score:39},
+    {name:"Sourav",score:40},
+    {name:"Talib",score:76},
+    {name:"Husen",score:80},
+    {name:"Savarna",score:96},
+    {name:"Karthik",score:95},
+    {name:"wasim",score:100},
+  ])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {students.map(student=>{
+        if(student.score>75){
+          if(student.score>=90){
+            return (
+              <p className="bg-pink">{student.name} :{student.score}</p>
+            )
+          }else{
+            return (
+              <p>{student.name} :{student.score}</p>
+            )
+          }
+        }
+      })}
+      
+      
     </div>
   );
 }
